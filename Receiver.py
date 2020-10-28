922870217684598359371276a90f915c44995ba3
crigonza <crigonza@adobe.com>
2020-10-27 21:11:30 -0500

    Functional code

--- Receiver.py
+++ Receiver.py
@@ -1,10 +1,14 @@
+import hashlib
 import socket
 import struct
+import select
 import sys
+import os
 
-ack = 'ack'
+
 multicast_group = '224.3.29.71'
 server_address = ( '', 10000 )
+timeout = 0.2
 
 # Create the socket
 sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
@@ -18,13 +22,40 @@ group = socket.inet_aton( multicast_group )
 mreq = struct.pack( '4sl', group, socket.INADDR_ANY )
 sock.setsockopt( socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq )
 
-# Receive/respond loop
 while True:
-    print( '\nwaiting to receive message' )
-    data, address = sock.recvfrom( 1024 )
+    data, addr = sock.recvfrom(1024)
+    data = data.decode("utf-8")
+    if data:
+        print("File name:" + data)
+        file_name = os.path.basename(data)
 
-    print( 'Received %s bytes from %s' % ( len( data ), address ))
-    print( data )
+    f = open(file_name, 'wb')
 
-    print( 'Sending acknowledgement to', address )
-    sock.sendto( ack.encode( "ascii" ), address )
+    data, addr = sock.recvfrom(1024)
+    data = data.decode("utf-8")
+    if data:
+        print("Hash:" + data)
+        file_hash = data.strip()
+
+    sock.sendto('OK'.encode('utf-8'), addr)
+
+    print('Receiving...')
+    while True:
+        ready = select.select([sock], [], [], timeout)
+        if ready[0]:
+            data, addr = sock.recvfrom(1024)
+            f.write(data)
+            
+        else:
+            f.close()
+            print("%s Finish!" % file_name)
+            print( 'Received %s bytes from %s' % (os.path.getsize(file_name), addr))
+            break
+    
+    # Send file hash to the multicast group
+    with open(file_name, "rb") as f:
+        if file_hash == hashlib.md5(f.read()).hexdigest():
+            sock.sendto('1'.encode('utf-8'), addr)
+        else:
+            sock.sendto('0'.encode('utf-8'), addr)
+    break  
--- Sender.py
+++ Sender.py
@@ -1,8 +1,34 @@
+import argparse
+import hashlib
 import socket
 import struct
+import time
 import sys
+from tqdm import tqdm
+import os
+from datetime import datetime
 
-message = 'Mensaje En Multidifusión'
+parser = argparse.ArgumentParser(
+    description="File transfer UDP server"
+)
+
+parser.add_argument(
+    "--file", default=1, type=int, help="file id to be send"
+)
+
+parser.add_argument(
+    "--num_clients", default=1, type=int, help="number of conections to receive",
+)
+
+args = parser.parse_args()
+
+args_dict = vars(args)
+print("Argument list to program")
+print(
+    "\n".join(["--{0} {1}".format(arg, args_dict[arg]) for arg in args_dict])
+)
+print("\n\n")
+
 multicast_group = ( '224.3.29.71', 10000 )
 
 # Create the datagram socket
@@ -10,31 +36,74 @@ sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
 # Set a timeout so the socket does not block indefinitely when trying
 # to receive data.
-sock.settimeout( 0.2 )
+# sock.settimeout( 0.2 )
 
 # Set the time-to-live for message to 1 so they don't go past the
 # the local network segment
 ttl = struct.pack( 'b', 1 )
-sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl )
+sock.setsockopt( socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+buf = 1024
+file_dict = {
+    1: './data/file1.bin',
+    2: './data/file2.bin'
+}
+file_name = file_dict[args.file]
 
 try:
+    now = datetime.now()
+    dt_string = now.strftime("%d-%m-%Y---%H-%M-%S")
+    log_name = "log"+dt_string
+    log = open(log_name+".txt", "x")
+    log.write("Prueba con fecha y hora de " + dt_string + "\n")
+    tam = os.path.getsize(file_name)
+    log.write("Archivo " + file_name + " de tamaño " + str(tam) + "\n")
 
-    # Send data to the multicast group
-    print( 'Enviando "%s"' % message )
 
-    sent = sock.sendto( message.encode( "ascii" ), multicast_group )
+    # Send filename to the multicast group
+    sock.sendto(file_name.encode('utf-8'), multicast_group)
+    print("Sending %s ..." % file_name)
+
+    # Send file hash to the multicast group
+    with open(file_name, "rb") as f:
+        file_hash = hashlib.md5(f.read()).hexdigest()
+    sock.sendto(file_hash.encode('utf-8'), multicast_group)
 
     # Look for responses from all recipients
-    while True:
-        print( 'Esperando respuesta' )
-        try:
-            data, server = sock.recvfrom( 16 )
-        except socket.timeout:
-            print( 'Time out, no se recibieron mas respuestas')
-            break
-        else:
-            print( 'Recibido "%s" de "%s"' % ( data, server ))
+    print( 'Esperando notificaciones de que los clientes están listos para recibir el archivo' )
+    count = 0
+    while True and count < args.num_clients:
+        data, server = sock.recvfrom(1024)
+        log.write("Llego cliente #"+str(count) + "\n")
+        print( 'Recibido "%s" de "%s"' % ( data, server ))
+        count += 1
+    count = 0
+    
+    # Send data to the multicast group
+    f = open(file_name, "rb")
+    data = f.read(buf)
+    pbar = tqdm(total=int(os.path.getsize(file_name)/buf))
+    start = time.time()
+    while(data):
+        if(sock.sendto(data, multicast_group)):
+            #  log.write("Enviando paquete " + str(count))
+            count+=1
+            data = f.read(buf)
+            pbar.update(1)
+    end = time.time()
+    log.write("Tiempo en transferir " + str(end - start) + " segundos\n")
+    f.close()
+    pbar.close()
 
+    # Look for responses from all recipients
+    print( 'Esperando respuesta' )
+    count = 0
+    while True and count < args.num_clients:
+        data, server = sock.recvfrom(1024)
+        print( 'Recibido "%s" de "%s"' % ( data, server ))
+        log.write('Cliente "%s" responde con integridad "%s"\n' % (count,data))
+        count += 1
+    log.close()
 finally:
     print( 'Socket cerrado' )
     sock.close()
